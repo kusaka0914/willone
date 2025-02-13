5cb6e3b2a5fabe96198eea5e692a7da4d5f6f95c
Takumi Kusaka <your.email@example.com>
2025-02-13 15:12:40 +0900

    修正

--- resources/views/sp/mainparts/newjob.blade.php
+++ resources/views/sp/mainparts/newjob.blade.php
@@ -6,15 +6,25 @@
                         <h3 class="c-job-title matchHeight"><a href="{{ route('OpportunityDetail' , ['id' => $new_data['job_id'] ]) }}" class="c-job-link">{{ $new_data['office_name'] }}の求人情報</a><span class="c-job-catch">{{ $new_data['order_pr_title'] }}</span></h3>
                         <dl class="c-job-detail">
                             <dt>勤務地</dt>
-                            <dd>{{ $new_data['addr'] }}</dd>
+                            <div class="c-job-detail-wrap">
+                                <dd>{{ $new_data['addr'] }}</dd>
+                            </div>
                             <dt>募集職種</dt>
-                            <dd>{{ $new_data['job_type_name'] }}</dd>
+                            <div class="c-job-detail-wrap">
+                                <dd>{{ $new_data['job_type_name'] }}</dd>
+                            </div>
                             <dt>最寄駅</dt>
-                            <dd>{{ $new_data['station1'] }}</dd>
+                            <div class="c-job-detail-wrap">
+                                <dd>{{ $new_data['station1'] }}</dd>
+                            </div>
                             <dt>給与</dt>
-                            <dd>{{ $new_data['salary'] }}</dd>
+                            <div class="c-job-detail-wrap">
+                                <dd>{{ $new_data['salary'] }}</dd>
+                            </div>
                             <dt>休日・休暇</dt>
-                            <dd>{{ $new_data['dayoff'] }}</dd>
+                            <div class="c-job-detail-wrap">
+                                <dd>{{ $new_data['dayoff'] }}</dd>
+                            </div>
                         </dl>
                     </li>
                     @endforeach
